eb93f23c0520e5ed66bf3e4f92710985181d611f
Vivien Schilis <vivien.schilis@gmail.com>
2009-06-06 14:07:27 +0200

    comment with_scope for production

--- app/models/user_session.rb
+++ app/models/user_session.rb
@@ -4,8 +4,8 @@ class UserSession < Authlogic::Session::Base
   remember_me_for 1.weeks
   self.find_by_login_method= "find_by_login_or_email"
   
-  with_scope(:find_options => {:conditions => ["account_id = ?", @account_id]}) do
-    find
-  end
+  # with_scope(:find_options => {:conditions => ["account_id = ?", @account_id]}) do
+  #   find
+  # end
   
 end
